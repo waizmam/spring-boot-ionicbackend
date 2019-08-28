e5b65c00a4b710ad13f7da0ff7f086129f3146d9
waizmam <waizmam.rj@gmail.com>
2019-08-28 11:58:35 -0300

    estado e Cidade

---
+++ src/main/java/com/waizmam/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.waizmam.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.waizmam.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
--- src/main/java/com/waizmam/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/waizmam/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.waizmam.cursomc.resources.exceptions;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,7 +11,7 @@ import com.waizmam.cursomc.services.exceptions.ObjectNotFoundException;
 public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e){
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
