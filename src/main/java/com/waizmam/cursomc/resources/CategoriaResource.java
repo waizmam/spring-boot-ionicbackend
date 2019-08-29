df219a74e0cf34fae18f6443dae97ce1c7d3755c
waizmam <waizmam.rj@gmail.com>
2019-08-29 12:30:01 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/waizmam/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/waizmam/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.waizmam.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.waizmam.cursomc.domain.Categoria;
+import com.waizmam.cursomc.dto.CategoriaDTO;
 import com.waizmam.cursomc.services.CategoriaService;
 
 @RestController
@@ -35,6 +38,7 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
 		obj.setId(id);
@@ -46,6 +50,13 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {		
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {		
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto) ;
 	}
 
 }
--- src/main/java/com/waizmam/cursomc/services/CategoriaService.java
+++ src/main/java/com/waizmam/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.waizmam.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,10 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
@@ -41,7 +46,6 @@ public class CategoriaService {
 		}
 		catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
-		}
-		
+		}		
 	}
 }
