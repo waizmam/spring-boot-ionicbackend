3418492c130ea1a2f3d00083c1da4c0dc0403112
waizmam <waizmam.rj@gmail.com>
2019-08-30 10:49:53 -0300

    Personalizando o retorno da Validação

--- src/main/java/com/waizmam/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/waizmam/cursomc/dto/CategoriaDTO.java
@@ -15,7 +15,7 @@ public class CategoriaDTO implements Serializable {
 	private Integer id;
 	
 	@NotEmpty(message = "Preenchimento obrigatório")
-	@Length(min = 5, max = 80, message = "Entre 5 e 80 caracteres")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
